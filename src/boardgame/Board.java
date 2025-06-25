0d694bb7c836aeb820afd65345c9fa27d9678067
Guilherme Cordeiro <guitocordeiro2003@gmail.com>
2025-06-25 12:57:28 -0300

    Programação defensiva e validações referentes a posições do tabuleiro ocupadas ou inexistentes

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(Integer rows, Integer columns) {
+        if(rows < 1 || columns < 1){
+            throw new BoardException("O tabuleiro deve ter, pelo menos, 1 linha e 1 coluna");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -29,15 +32,39 @@ public class Board {
     }
     
     public Piece piece(int row, int column){
+        if(!positionExists(row, column)){
+            throw new BoardException("Posicao inexistente");
+        }
         return pieces[row][column];
     }
     
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Posicao inexistente");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("Ja tem uma peça na posicao " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+    
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+    
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+    
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Posicao inexistente");
+        }
+        return piece(position) != null;
     }
 }
